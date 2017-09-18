06bfbd7b4fafbdcff65c5ebab75b86a4f519410d
Simone de Blasiis <simone.deblasiis@gmail.com>
2017-09-18 16:34:29 +0200

    Il DB è vivo!!!

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 create table categorie (
-  Nome varchar(50) not null AUTO_INCREMENT PRIMARY KEY,
+  Nome varchar(50) not null PRIMARY KEY,
   Descrizione varchar(255)
 );
 
@@ -15,76 +15,95 @@ create table utente (
 );
 create table venditore (
   IdVenditore int AUTO_INCREMENT primary key,
-  IdUtente int not null foreign key references utente(IdUtente),
+  IdUtente int not null,
   NomeNegozio varchar(50),
   RegioneSociale varchar(50),
   PartitaIva varchar(11),
   Iban varchar(12),
   Indirizzo varchar(100),
-  Valutazione float
+  Valutazione double,
+  foreign key (IdUtente) references utente(IdUtente)
 );
 create table articolo (
   IdArticolo int AUTO_INCREMENT primary key,
   Nome varchar(100) not null,
-  IdVenditore int not null foreign key references venditore(IdVenditore),
+  IdVenditore int not null,
   Prezzo float,
-  Categoria varchar(50) not null foreign key references categoria(Nome),
-  Voto float
+  Categoria varchar(50) not null,
+  Voto float,
+  foreign key (Categoria) references categorie(Nome),
+  foreign key (IdVenditore) references venditore(IdVenditore)
 );
 create table recensioneArticoli (
   IdRecArt int AUTO_INCREMENT PRIMARY KEY,
-  IdUtente int foreign key references utente(IdUtente),
-  Voto BETWEEN 0 and 5,
+  IdUtente int,
+  Voto int,
   Testo varchar(255),
-  Visualizzato boolean,
-  IdArticolo int foreign key references articolo(IdArticolo)
+  Visualizzato enum("TRUE","FALSE"),
+  IdArticolo int,
+  foreign key (IdArticolo) references articolo(IdArticolo),
+  foreign key (IdUtente) references utente(IdUtente)
 );
 
 create table recensioneVenditore (
   IdRecVen int AUTO_INCREMENT primary key,
-  IdUtente int foreign key references utente(IdUtente),
-  voto BETWEEN 0 and 5,
+  IdUtente int,
+  voto int,
   Testo varchar(255),
   Visualizzato boolean,
-  IdVenditore int foreign key references venditore(IdVenditore)
+  IdVenditore int,
+  foreign key (IdVenditore) references venditore(IdVenditore),
+  foreign key (IdUtente) references utente(IdUtente)
 );
 
 create table ImmaginiUtente (
   IdImmagine int AUTO_INCREMENT primary key,
-  Path varchar(255) not null unique,
-  IdUtente int foreign key references utente(IdUtente)
-)
+  Percorso varchar(255) not null unique,
+  IdUtente int,
+  foreign key (IdUtente) references utente(IdUtente)
+);
 create table ImmaginiVenditore (
   IdImmagine int AUTO_INCREMENT primary key,
-  Path varchar(255) not null unique,
-  IdVenditore int foreign key references venditore(IdVenditore)
-)
+  Percorso varchar(255) not null unique,
+  IdVenditore int,
+  foreign key (IdVenditore) references venditore(IdVenditore)
+);
 create table ImmaginiArticoli (
   IdImmagine int AUTO_INCREMENT primary key,
-  Path varchar(255) not null unique,
-  IdArticolo int foreign key references articolo(IdArticolo)
+  Percorso varchar(255) not null unique,
+  IdArticolo int,
+  foreign key (IdArticolo) references articolo(IdArticolo)
 );
 create table ordine (
   IdOrdine int AUTO_INCREMENT primary key,
-  IdVenditore int foreign key references venditore(IdVenditore),
-  IdUtente int foreign key references utente(IdUtente),
+  IdVenditore int,
+  IdUtente int,
   PrezzoTot float,
   DataSpedizione date,
   PagamentoRicevuto boolean not null,
-  Ricevuto boolean not null
+  Ricevuto boolean not null,
+  foreign key (IdVenditore) references venditore(IdVenditore),
+  foreign key (IdVenditore) references utente(IdUtente)
 );
 create table articoloOrdine (
-  IdOrdine int foreign key references ordine(IdOrdine),
-  IdArticolo int foreign key references articolo(IdArticolo),
+  IdOrdine int,
+  IdArticolo int,
+  foreign key (IdOrdine) references ordine(IdOrdine),
+  foreign key (IdArticolo) references articolo(IdArticolo),
   primary key (IdOrdine, IdArticolo)
 );
 create table ticket (
   IdTicket int AUTO_INCREMENT primary key,
   Titolo varchar(100) not null,
-  IdOrdine int foreign key references ordine(IdOrdine),
-  IdArticolo int foreign key references articolo(IdArticolo),
-  IdVenditore int foreign key references venditore(IdVenditore),
-  IdUtente int foreign key references utente(IdUtente),
+  IdOrdine int,
+  IdArticolo int,
+  IdVenditore int,
+  IdUtente int,
   Testo varchar(255) not null,
-  Stato enum ('Aperto', 'In Lavorazione', 'Chiuso con successo', 'Annullato')
+  Stato enum ("Aperto", "In Lavorazione", "Chiuso con successo", "Annullato"),
+  foreign key (IdOrdine) references ordine(IdOrdine),
+  foreign key (IdArticolo) references articolo(IdArticolo),
+  foreign key (IdVenditore) references venditore(IdVenditore),
+  foreign key (IdUtente) references utente(IdUtente)
 );
+

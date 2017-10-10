7dc9f6101b2fd2f45f35e1bbe79895c9d5e8ab05
Simone de Blasiis <simone.deblasiis@gmail.com>
2017-10-10 19:15:19 +0200

    Aggiunto toString a TipoUtente

--- src/main/java/it/unitn/progettoweb/Objects/TipoUtente.java
+++ src/main/java/it/unitn/progettoweb/Objects/TipoUtente.java
@@ -1,5 +1,25 @@
 package it.unitn.progettoweb.Objects;
 
 public enum TipoUtente {
-    USER,SELLER,ADMIN,ERROR
+    USER,SELLER,ADMIN,ERROR;
+
+    @Override
+    public String toString() {
+        String result = "";
+        switch (this) {
+            case USER:
+                result = "User";
+                break;
+            case SELLER:
+                result = "Seller";
+                break;
+            case ADMIN:
+                result = "Admin";
+                break;
+            case ERROR:
+                result = "Error";
+                break;
+        }
+        return result;
+    }
 }

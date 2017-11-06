6a25a6f13449f0f82ad4fadeb03e6edfc3d53d80
dema96 <fededema96@gmail.com>
2017-11-06 17:47:36 +0100

    Sistemato query insert

--- query insert.sql
+++ query insert.sql
@@ -1,13 +1,13 @@
-﻿INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`) VALUES ('Dema', 'dema1', 'Federico', 'De Martin', 'fede@dema.it', '1996-10-12', 'Seller', 'true', 'asdasdas');
-INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`) VALUES ('Giulia', 'giu1', 'Giulia', 'Nardò', 'giu@nardo.it', '1996-08-12', 'Admin', 'true', 'asdasdas');
-INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`) VALUES ('Debla', 'debla1', 'Simone', 'De Blasiis', 'simo@debla.it', '1996-02-12', 'User', 'true', 'asdasdas');
-INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`) VALUES ('Simo', 'simo1', 'Simone', 'De Cassan', 'simo@deca.it', '1998/08/16', 'Seller', 'true', 'asdasdas');
-INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`) VALUES ('Giada', 'giada1', 'Giada', 'Nunzia', 'giada@nunzia.it', '1994/05/14', 'Seller', 'true', 'asdasdas');
-INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`) VALUES ('Giacoz', 'giacoz1', 'Giacomo', 'Mazzini', 'giacoz@maz.it', '1990/05/08', 'User', 'true', 'asdasdas');
+﻿INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`,`Validato`,`validationhash`) VALUES ('Dema', 'dema1', 'Federico', 'De Martin', 'fede@dema.it', '1996-10-12', 'Seller', 'true', 'asdasdas');
+INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`,`Validato`,`validationhash`) VALUES ('Giulia', 'giu1', 'Giulia', 'Nardò', 'giu@nardo.it', '1996-08-12', 'Admin', 'true', 'asdasdas');
+INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`,`Validato`,`validationhash`) VALUES ('Debla', 'debla1', 'Simone', 'De Blasiis', 'simo@debla.it', '1996-02-12', 'User', 'true', 'asdasdas');
+INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`,`Validato`,`validationhash`) VALUES ('Simo', 'simo1', 'Simone', 'De Cassan', 'simo@deca.it', '1998/08/16', 'Seller', 'true', 'asdasdas');
+INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`,`Validato`,`validationhash`) VALUES ('Giada', 'giada1', 'Giada', 'Nunzia', 'giada@nunzia.it', '1994/05/14', 'Seller', 'true', 'asdasdas');
+INSERT INTO `utente` (`UserName`, `Password`, `Nome`, `Cognome`, `Email`, `DataNascita`, `Tipo`,`Validato`,`validationhash`) VALUES ('Giacoz', 'giacoz1', 'Giacomo', 'Mazzini', 'giacoz@maz.it', '1990/05/08', 'User', 'true', 'asdasdas');
 
-INSERT INTO `venditore` (`IdVenditore`, `IdUtente`, `NomeNegozio`, `RegioneSociale`, `PartitaIva`, `Iban`, `Indirizzo`, `Valutazione`) VALUES (NULL, '4', 'Simoncelli', 'SPA', '15478972300', '000510258741', 'Via simo 5', NULL);
-INSERT INTO `venditore` (`IdVenditore`, `IdUtente`, `NomeNegozio`, `RegioneSociale`, `PartitaIva`, `Iban`, `Indirizzo`, `Valutazione`) VALUES (NULL, '1', 'Dema Shop', 'SRL', '02154879632', '125489632755', 'Via dema 8', NULL);
-INSERT INTO `venditore` (`IdVenditore`, `IdUtente`, `NomeNegozio`, `RegioneSociale`, `PartitaIva`, `Iban`, `Indirizzo`, `Valutazione`) VALUES (NULL, '5', 'GIAA', 'SRL', '12589636547', '103200587960', 'Via G 9', NULL);
+INSERT INTO `venditore` (`IdVenditore`, `IdUtente`, `NomeNegozio`, `RagioneSociale`, `PartitaIva`, `Iban`, `Indirizzo`, `Valutazione`) VALUES (NULL, '4', 'Simoncelli', 'SPA', '15478972300', '000510258741', 'Via simo 5', NULL);
+INSERT INTO `venditore` (`IdVenditore`, `IdUtente`, `NomeNegozio`, `RagioneSociale`, `PartitaIva`, `Iban`, `Indirizzo`, `Valutazione`) VALUES (NULL, '1', 'Dema Shop', 'SRL', '02154879632', '125489632755', 'Via dema 8', NULL);
+INSERT INTO `venditore` (`IdVenditore`, `IdUtente`, `NomeNegozio`, `RagioneSociale`, `PartitaIva`, `Iban`, `Indirizzo`, `Valutazione`) VALUES (NULL, '5', 'GIAA', 'SRL', '12589636547', '103200587960', 'Via G 9', NULL);
 
 INSERT INTO `categorie` (`Nome`, `Descrizione`) VALUES ('Smartphone', NULL);
 INSERT INTO `categorie` (`Nome`, `Descrizione`) VALUES ('Elettrodomestici', NULL);

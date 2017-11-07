ae2ea7678b26d7f3df238a4c0f26485fd245434c
dema96 <fededema96@gmail.com>
2017-11-07 15:01:15 +0100

    Aggiunta tabella sessionUser

--- db.sql
+++ db.sql
@@ -3,6 +3,7 @@ create table categorie (
   Descrizione varchar(255)
 );
 
+
 create table utente (
   IdUtente int AUTO_INCREMENT primary key,
   UserName varchar(50) not null,
@@ -14,6 +15,12 @@ create table utente (
   Tipo enum('Administrator', 'User', 'Seller') not null,
   Validato enum('true', 'false')not null,
   validationhash varchar(25) not null
+);
+create table sessionUser(
+  IdSession int AUTO_INCREMENT primary key,
+  username varchar(50) not null,
+  dataSession date,
+  hashcode varchar(50)
 );
 create table venditore (
   IdVenditore int AUTO_INCREMENT primary key,
@@ -110,4 +117,5 @@ create table ticket (
   foreign key (IdVenditore) references venditore(IdVenditore),
   foreign key (IdUtente) references utente(IdUtente)
 );
+
 
